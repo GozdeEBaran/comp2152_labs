84c8c0ed1ad36b733256acb7b3a39a44646d3322
GozdeEBaran <eski.gozdee@gmail.com>
2025-03-28 08:40:16 -0400

    person.py and tick.py code added

--- week12/person.py
+++ week12/person.py
@@ -1,6 +1,9 @@
+
 from mammal import Mammal
 
 # Lab 12 - Child can inherit only public fields from the parent
+
+
 class Person(Mammal):
     # fields from Parent
     # age
@@ -14,18 +17,25 @@ class Person(Mammal):
 
         # Set the Person-specific fields
         print("Constructor: Adding the Person parts of a person")
-
+        self.name = p_name
+        self.__height = p_height
 
     def __del__(self):
         print("Destructor: The garbage collector is now deleting the person object")
-        
+        super(Person, self).__del__()
 
         # Complex getter for height
-
+    @property
+    def height(self):
+        return self.__height
     # Complex setter for height
 
+    @height.setter
+    def height(self, p_height):
+        self.__height = p_height
 
     # Lab 12 - Private or protected METHODS are not possible in python.
     # Lab 12 - This means that all methods are public and can be overridden in a derived class.
+
     def love(self):
         print("This human is feeling love in a more complex emotional way...")
